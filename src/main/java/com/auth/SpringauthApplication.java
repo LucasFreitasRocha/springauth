b5f9a6f482f9d1b35e96626962099fc785db1ad3
Lucas Rocha <rochadefreitaslucas@gmail.com>
2021-04-27 22:34:19 -0300

    inital config

---
+++ src/main/java/com/auth/SpringauthApplication.java
@@ -0,0 +1,13 @@
+package com.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringauthApplication.class, args);
+	}
+
+}
